9a06094d7ccbe250403842e0c305a6cd9d0af31a
binhle410 <binh@sunrise.vn>
2019-09-09 17:49:19 +0700

    hello motor

---
+++ api/src/Security/DecisionMakingInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Security;
+
+interface DecisionMakingInterface {
+	
+	const DECISION_APPROVE = 'APPROVE';
+    const DECISION_RESET= 'RESET';
+	const DECISION_REJECT = 'REJECT';
+	
+	const STATUS_NEW = 'NEW';
+	const STATUS_EXPIRED = 'EXPIRED';
+	const STATUS_REJECTED = 'REJECTED';
+	const STATUS_APPROVED = 'APPROVED';
+	
+	
+	public function getDecisionStatus(): string;
+	
+	public function makeDecision(string $decision);
+	
+	public function setDecisionRemarks(?string $remarks);
+	
+	public function getDecisionRemarks(): ?string;
+	
+}
