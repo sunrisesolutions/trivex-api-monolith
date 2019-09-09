ea0133db4db25f737a7f639fd13a4e9c7098040c
binhle410 <binh@sunrise.vn>
2019-09-09 23:55:28 +0700

    hello motor

---
+++ api/src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\User\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class JWTCreatedListener
+{
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    /**
+     * @param RequestStack $requestStack
+     */
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * @param JWTCreatedEvent $event
+     */
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        /** @var User $user */
+        $user = $event->getUser();
+        $payload = $event->getData();
+
+        if (!empty($request)) {
+            $payload['ip'] = $request->getClientIp();
+
+
+            $payload['org'] = $request->attributes->get('orgUid');
+            $payload['im'] = $request->attributes->get('imUid');
+
+            if (empty($payload['org'])) {
+                $imUuid = $request->request->get('im-uuid');
+                if (empty($imUuid)) {
+                    /** @var OrganisationUser $im */
+                    $im = $user->getOrganisationUsers()->first();
+                    $imUuid = $im->getUuid();
+                    $orgUuid = $im->getOrganisation()->getUuid();
+                } else {
+                    $im = $user->findOrgUserByUuid($imUuid);
+                    $orgUuid = $im->getOrganisation()->getUuid();
+                }
+                $payload['org'] = $orgUuid;
+                $payload['im'] = $imUuid;
+            }
+        } else {
+            $payload['ip'] = null;
+        }
+//        $payload['uuid'] = $user->getUuid();
+
+        $event->setData($payload);
+
+//        $header = $event->getHeader();
+//        $header['cty'] = 'JWT';
+//
+//        $event->setHeader($header);
+    }
+}
