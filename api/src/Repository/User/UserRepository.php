689ae561c9a654b7c7a5b7d92682019ae6a4a981
binhle410 <binh@sunrise.vn>
2019-09-29 21:09:20 +0700

    ($birthday)

--- api/src/Repository/User/UserRepository.php
+++ api/src/Repository/User/UserRepository.php
@@ -29,7 +29,7 @@ class UserRepository extends ServiceEntityRepository
         return $qb
             ->andWhere(
                 $expr->andX(
-                    $expr->eq('u.birthDate', ($birthday)),
+                    $expr->eq('u.birthDate', $expr->literal($birthday)),
                     $expr->like('u.phone', $expr->literal('%'.$phone))
                 )
             )
